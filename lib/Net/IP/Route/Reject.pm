6ee96e709c2edf6a5cdb568ebaba5c51fb4418c2
Dana Hudes <dhudes@hudes.org>
2003-12-16 07:45:27 +0000

    Import of DHUDES/Net-IP-Route-Reject-0.4 from CPAN.
    gitpan-cpan-distribution: Net-IP-Route-Reject
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         DHUDES/Net-IP-Route-Reject-0.4.tar.gz
    gitpan-cpan-author:       DHUDES
    gitpan-cpan-maturity:     released

--- lib/Net/IP/Route/Reject.pm
+++ lib/Net/IP/Route/Reject.pm
@@ -1,4 +1,4 @@
-package IP::Route::Reject;
+package Net::IP::Route::Reject;
 use strict;
 use Carp;
 use IPC::Cmd qw[can_run run];
@@ -6,7 +6,7 @@ use CLASS;
 BEGIN {
 	use Exporter ();
 	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION     = 0.3;
+	$VERSION     = 0.4;
 	@ISA         = qw (Exporter);
 	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw ();
--- t/001_load.t
+++ t/001_load.t
@@ -6,7 +6,9 @@ use Test::More tests => 2;
 
 BEGIN { use_ok( 'Net::IP::Route::Reject' ); }
 
-my $object = Net::IP::Route::Reject->new ();
-isa_ok ($object, 'Net::IP::Route::Reject');
+ Net::IP::Route::Reject->add ('1.1.1.1');#pass if it doesn't blow up
+#isa_ok ($object, 'Net::IP::Route::Reject');
+Net::IP::Route::Reject->del('1.1.1.1');
+pass("Add/Delete route 1.1.1.1");
 
 
